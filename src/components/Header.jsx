f983ee0b507281da1c6822689488cd39f5318389
Reese Parsons <reeseparsons99@gmail.com>
2025-10-28 15:38:25 -0300

    Remove unused components and styles from the project, including App.css, App.jsx, Education.jsx, Header.jsx, index.css, and Professional.jsx. Update import path for App component in main.jsx.

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="name-container">
+                <h1 className="name">Reese Parsons</h1>
+            </div>
+            <div className="contact">
+                <p>github</p>
+                <p>email</p>
+                <p>linkedin</p>
+            </div>
+        </header>
+    )
+}
